0441071aa0553f6ff7b42f182fd5a89bd90e8bb6
elkamelsallemi <elkamel.sallemi@gmail.com>
2023-12-11 20:04:22 +0100

    Adding Startup.cs file

---
+++ Phoenix.Models/Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Phoenix.Models
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
