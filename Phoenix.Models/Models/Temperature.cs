0556600aca99009f308e8f87d60b01d3e194ac05
elkamelsallemi <elkamel.sallemi@gmail.com>
2023-12-11 18:57:45 +0100

    Adding Models

---
+++ Phoenix.Models/Models/Temperature.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace Phoenix.Models.Models
+{
+    public class Temperature
+    {
+        [JsonPropertyName("CountryId")]
+        public string CountryId { get; set; }
+
+  
+        [JsonPropertyName("Temperature")]
+        public int Temp { get; set; }
+
+    }
+}
+
+
